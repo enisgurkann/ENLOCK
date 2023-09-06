1ee52f74bd6b8a192967558a6fa88e0ae298fb72
enis <enisgurkan01@gmail.com>
2023-09-06 10:43:38 +0300

    WithNoLock Add

--- EnLock/EnExtention.cs
+++ EnLock/EnExtention.cs
@@ -7,186 +7,228 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Transactions;
 
-namespace EnLock
+namespace EnLock;
+
+public static class EnExtention
 {
-    public static class EnExtention
+    public static async Task<bool> ToAnyWithNoLockAsync<T>(this IQueryable<T> query,
+        CancellationToken cancellationToken = default)
     {
-        public static async Task<bool> ToAnyWithNoLockAsync<T>(this IQueryable<T> query, CancellationToken cancellationToken = default)
+        bool result = default;
+        using (var scope = new TransactionScope(TransactionScopeOption.Required,
+                   new TransactionOptions()
+                   {
+                       IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
+                   },
+                   TransactionScopeAsyncFlowOption.Enabled))
         {
-            bool result = default;
-            using (var scope = new TransactionScope(TransactionScopeOption.Required,
-                                    new TransactionOptions()
-                                    {
-                                        IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
-                                    },
-                                    TransactionScopeAsyncFlowOption.Enabled))
-            {
-                result = await query.AnyAsync(cancellationToken);
-                scope.Complete();
-            }
-            return result;
+            result = await query.AnyAsync(cancellationToken);
+            scope.Complete();
         }
-        public static async Task<T[]> ToArrayWithNoLockAsync<T>(this IQueryable<T> query, CancellationToken cancellationToken = default)
-        {
-            T[] result = default;
-            using (var scope = new TransactionScope(TransactionScopeOption.Required,
-                                    new TransactionOptions()
-                                    {
-                                        IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
-                                    },
-                                    TransactionScopeAsyncFlowOption.Enabled))
-            {
-                result = await query.ToArrayAsync(cancellationToken);
-                scope.Complete();
-            }
-            return result;
-        }
-        public static async Task<List<T>> ToListWithNoLockAsync<T>(this IQueryable<T> query, CancellationToken cancellationToken = default)
-        {
-            List<T> result = default;
-            using (var scope = new TransactionScope(TransactionScopeOption.Required,
-                                    new TransactionOptions()
-                                    {
-                                        IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
-                                    },
-                                    TransactionScopeAsyncFlowOption.Enabled))
-            {
-                result = await query.ToListAsync(cancellationToken);
-                scope.Complete();
-            }
-            return result;
-        }
-        public static List<T> ToListWithNoLock<T>(this IQueryable<T> query)
-        {
-            List<T> result = default;
-            using (var scope = new TransactionScope(TransactionScopeOption.Required,
-                       new TransactionOptions()
-                       {
-                           IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
-                       },
-                       TransactionScopeAsyncFlowOption.Enabled))
-            {
-                result = query.ToList();
-                scope.Complete();
-            }
-            return result;
-        }
-        public static async Task<T> ToFirstOrDefaultWithNoLockAsync<T>(this IQueryable<T> query, CancellationToken cancellationToken = default)
-        {
-            T result = default;
-            using (var scope = new TransactionScope(TransactionScopeOption.Required,
-                                    new TransactionOptions()
-                                    {
-                                        IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
-                                    },
-                                    TransactionScopeAsyncFlowOption.Enabled))
-            {
-                result = await query.FirstOrDefaultAsync(cancellationToken);
-                scope.Complete();
-            }
-            return result;
-        }
-        public static async Task<T> ToFirstOrDefaultWithNoLockAsync<T>(this IQueryable<T> query, Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default)
-        {
-            T result = default;
-            using (var scope = new TransactionScope(TransactionScopeOption.Required,
-                                    new TransactionOptions()
-                                    {
-                                        IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
-                                    },
-                                    TransactionScopeAsyncFlowOption.Enabled))
-            {
-                result = await query.FirstOrDefaultAsync(predicate, cancellationToken);
-                scope.Complete();
-            }
-            return result;
-        }
-        public static async Task<T> ToFirstWithNoLockAsync<T>(this IQueryable<T> query,Expression<Func<T, bool>> predicate,  CancellationToken cancellationToken = default)
-        {
-            T result = default;
-            using (var scope = new TransactionScope(TransactionScopeOption.Required,
-                                    new TransactionOptions()
-                                    {
-                                        IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
-                                    },
-                                    TransactionScopeAsyncFlowOption.Enabled))
-            {
-                result = await query.FirstAsync(predicate,cancellationToken);
-                scope.Complete();
-            }
-            return result;
-        }
-        public static async Task<T> ToFirstWithNoLockAsync<T>(this IQueryable<T> query, CancellationToken cancellationToken = default)
-        {
-            T result = default;
-            using (var scope = new TransactionScope(TransactionScopeOption.Required,
-                       new TransactionOptions()
-                       {
-                           IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
-                       },
-                       TransactionScopeAsyncFlowOption.Enabled))
-            {
-                result = await query.FirstAsync(cancellationToken);
-                scope.Complete();
-            }
-            return result;
-        }
-        public static async Task<T> ToSingleWithNoLockAsync<T>(this IQueryable<T> query, CancellationToken cancellationToken = default)
-        {
-            T result = default;
-            using (var scope = new TransactionScope(TransactionScopeOption.Required,
-                                    new TransactionOptions()
-                                    {
-                                        IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
-                                    },
-                                    TransactionScopeAsyncFlowOption.Enabled))
-            {
-                result = await query.SingleAsync(cancellationToken);
-                scope.Complete();
-            }
-            return result;
-        }
-        
-        public static async Task<T> ToSingleWithNoLockAsync<T>(this IQueryable<T> query, Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default)
-        {
-            T result = default;
-            using (var scope = new TransactionScope(TransactionScopeOption.Required,
-                       new TransactionOptions()
-                       {
-                           IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
-                       },
-                       TransactionScopeAsyncFlowOption.Enabled))
-            {
-                result = await query.SingleAsync(predicate,cancellationToken);
-                scope.Complete();
-            }
-            return result;
-        }
-        
-        
-        #region NoLockFunc
-        /// <summary>
-        /// No Lock tag added
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <typeparam name="TDbContext"></typeparam>
-        /// <param name="dbContext"></param>
-        /// <param name="func"></param>
-        /// <returns></returns>
-        public static T NoLock<T, TDbContext>(this TDbContext dbContext, Func<TDbContext, T> func) where TDbContext : DbContext
-        {
-            T result = default;
-            var transactionOptions = new TransactionOptions
-            {
-                IsolationLevel = IsolationLevel.ReadUncommitted
-            };
-            using (var scope = new TransactionScope(TransactionScopeOption.Required, transactionOptions))
-            {
-                result = func(dbContext);
-                scope.Complete();
-            }
-            return result;
-        }
-        #endregion
+
+        return result;
     }
+
+    public static IQueryable<T> WithNoLock<T>(this IQueryable<T> query,
+        CancellationToken cancellationToken = default)
+    {
+        using var scope = new TransactionScope(TransactionScopeOption.Required,
+            new TransactionOptions()
+            {
+                IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
+            },
+            TransactionScopeAsyncFlowOption.Enabled);
+        return query;
+    }
+
+    public static async Task<T[]> ToArrayWithNoLockAsync<T>(this IQueryable<T> query,
+        CancellationToken cancellationToken = default)
+    {
+        T[] result = default;
+        using (var scope = new TransactionScope(TransactionScopeOption.Required,
+                   new TransactionOptions()
+                   {
+                       IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
+                   },
+                   TransactionScopeAsyncFlowOption.Enabled))
+        {
+            result = await query.ToArrayAsync(cancellationToken);
+            scope.Complete();
+        }
+
+        return result;
+    }
+
+    public static async Task<List<T>> ToListWithNoLockAsync<T>(this IQueryable<T> query,
+        CancellationToken cancellationToken = default)
+    {
+        List<T> result = default;
+        using (var scope = new TransactionScope(TransactionScopeOption.Required,
+                   new TransactionOptions()
+                   {
+                       IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
+                   },
+                   TransactionScopeAsyncFlowOption.Enabled))
+        {
+            result = await query.ToListAsync(cancellationToken);
+            scope.Complete();
+        }
+
+        return result;
+    }
+
+    public static List<T> ToListWithNoLock<T>(this IQueryable<T> query)
+    {
+        List<T> result = default;
+        using (var scope = new TransactionScope(TransactionScopeOption.Required,
+                   new TransactionOptions()
+                   {
+                       IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
+                   },
+                   TransactionScopeAsyncFlowOption.Enabled))
+        {
+            result = query.ToList();
+            scope.Complete();
+        }
+
+        return result;
+    }
+
+    public static async Task<T> ToFirstOrDefaultWithNoLockAsync<T>(this IQueryable<T> query,
+        CancellationToken cancellationToken = default)
+    {
+        T result = default;
+        using (var scope = new TransactionScope(TransactionScopeOption.Required,
+                   new TransactionOptions()
+                   {
+                       IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
+                   },
+                   TransactionScopeAsyncFlowOption.Enabled))
+        {
+            result = await query.FirstOrDefaultAsync(cancellationToken);
+            scope.Complete();
+        }
+
+        return result;
+    }
+
+    public static async Task<T> ToFirstOrDefaultWithNoLockAsync<T>(this IQueryable<T> query,
+        Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default)
+    {
+        T result = default;
+        using (var scope = new TransactionScope(TransactionScopeOption.Required,
+                   new TransactionOptions()
+                   {
+                       IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
+                   },
+                   TransactionScopeAsyncFlowOption.Enabled))
+        {
+            result = await query.FirstOrDefaultAsync(predicate, cancellationToken);
+            scope.Complete();
+        }
+
+        return result;
+    }
+
+    public static async Task<T> ToFirstWithNoLockAsync<T>(this IQueryable<T> query,
+        Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default)
+    {
+        T result = default;
+        using (var scope = new TransactionScope(TransactionScopeOption.Required,
+                   new TransactionOptions()
+                   {
+                       IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
+                   },
+                   TransactionScopeAsyncFlowOption.Enabled))
+        {
+            result = await query.FirstAsync(predicate, cancellationToken);
+            scope.Complete();
+        }
+
+        return result;
+    }
+
+    public static async Task<T> ToFirstWithNoLockAsync<T>(this IQueryable<T> query,
+        CancellationToken cancellationToken = default)
+    {
+        T result = default;
+        using (var scope = new TransactionScope(TransactionScopeOption.Required,
+                   new TransactionOptions()
+                   {
+                       IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
+                   },
+                   TransactionScopeAsyncFlowOption.Enabled))
+        {
+            result = await query.FirstAsync(cancellationToken);
+            scope.Complete();
+        }
+
+        return result;
+    }
+
+    public static async Task<T> ToSingleWithNoLockAsync<T>(this IQueryable<T> query,
+        CancellationToken cancellationToken = default)
+    {
+        T result = default;
+        using (var scope = new TransactionScope(TransactionScopeOption.Required,
+                   new TransactionOptions()
+                   {
+                       IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
+                   },
+                   TransactionScopeAsyncFlowOption.Enabled))
+        {
+            result = await query.SingleAsync(cancellationToken);
+            scope.Complete();
+        }
+
+        return result;
+    }
+
+    public static async Task<T> ToSingleWithNoLockAsync<T>(this IQueryable<T> query,
+        Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default)
+    {
+        T result = default;
+        using (var scope = new TransactionScope(TransactionScopeOption.Required,
+                   new TransactionOptions()
+                   {
+                       IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted
+                   },
+                   TransactionScopeAsyncFlowOption.Enabled))
+        {
+            result = await query.SingleAsync(predicate, cancellationToken);
+            scope.Complete();
+        }
+
+        return result;
+    }
+
+
+    #region NoLockFunc
+
+    /// <summary>
+    /// No Lock tag added
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="TDbContext"></typeparam>
+    /// <param name="dbContext"></param>
+    /// <param name="func"></param>
+    /// <returns></returns>
+    public static T NoLock<T, TDbContext>(this TDbContext dbContext, Func<TDbContext, T> func)
+        where TDbContext : DbContext
+    {
+        T result = default;
+        var transactionOptions = new TransactionOptions
+        {
+            IsolationLevel = IsolationLevel.ReadUncommitted
+        };
+        using (var scope = new TransactionScope(TransactionScopeOption.Required, transactionOptions))
+        {
+            result = func(dbContext);
+            scope.Complete();
+        }
+
+        return result;
+    }
+
+    #endregion
 }
